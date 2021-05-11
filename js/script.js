b2c80b6ef9bcfaa3c8c713441ec7e9eb7306fe53
Goraychenok <55753151+Goraychenok@users.noreply.github.com>
2021-05-12 03:24:25 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
 jQuery(document).ready(function() {
 var swiper = new Swiper('.swiper-container', {
     slidesPerView: 1,
-      spaceBetween: 30,
+    
       loop: true,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
+      cssMode: true,
       navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
       },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      mousewheel: true,
+      keyboard: true,
   });
   $('.header__burger').click(function(event) {
     $('.header__burger,.header__menu').toggleClass('active');
@@ -19,7 +21,36 @@ var swiper = new Swiper('.swiper-container', {
 });
 
 
+$('#seoButton').click(function(event) {
+    $('.seo-link').addClass('none');
+    $('.seo-shadow').removeClass('none');
+})
 
+});
 
+$(function() {
+    
+    var headerTwice = $('.header').height();
+    var headerHeight = $('.header-twice').height();
+    var blockTop = $('.header').offset().top;
+    var blockBot =  $('.header-twice').height();
+    var CountUpFlag = 0;
+    var $window = $(window);
+   
+    console.log(blockBot);
+    $window.on('load scroll', function() {
+        var top = $window.scrollTop();
+        var height = $window.height() - blockBot - headerTwice;
+        
+        if (top +   headerTwice >= height && CountUpFlag == 0) {
+            console.log(top +   headerTwice);
+            console.log(headerHeight);
+            CountUp();
+            CountUpFlag = 1;
+        }
+    });
+    function CountUp() {
+        $('.header-three').addClass('test');
+    }
 });
 
